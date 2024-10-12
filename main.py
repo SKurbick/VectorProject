c845b2ca86f34e559ce36e15e36f68374ce061cd
SKurbick <kurban.63.axmedov@gmail.com>
2024-10-12 17:37:14 +0300

    [~]

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def schedule_tasks():
     schedule.every(25).minutes.do(lambda: asyncio.create_task(gs_service.add_new_day_revenue_to_table()))
 
     """Актуализация информации по ценам, скидкам, габаритам, комиссии, логистики от склада WB до ПВЗ"""
-    schedule.every(800).seconds.do(lambda: asyncio.create_task(run_in_executor(gs_service.add_actually_data_to_table)))
+    schedule.every(800).seconds.do(lambda: asyncio.create_task(gs_service.add_actually_data_to_table()))
     # schedule.every(1).seconds.do(lambda: asyncio.create_task(run_in_executor(gs_service.add_actually_data_to_table)))
 
     """Смотрит в таблицу, оценивает новые nm_ids"""
