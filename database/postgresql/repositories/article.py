fcf6e53f0281cf56311d1f6c677538333cecbeef
SKurbick <kurban.63.axmedov@gmail.com>
2024-10-13 14:13:37 +0300

    [*] добавление исключений для ненайденных вендор кодов по артиклам

--- database/postgresql/repositories/article.py
+++ database/postgresql/repositories/article.py
@@ -3,12 +3,14 @@ import asyncpg
 
 class ArticleTable:
     """Таблица article"""
+
     def __init__(self, db):
         self.db = db
 
     async def check_nm_ids(self, account: str, nm_ids: list):
         """Возвращает артикулы которых нет в таблице article"""
         nm_ids_str = ', '.join(f"({nm_id})" for nm_id in nm_ids)
+        print("check_nm_ids",nm_ids_str)
         query = f"""
         SELECT nm_id
         FROM (VALUES {nm_ids_str}) AS input(nm_id)
@@ -25,12 +27,34 @@ class ArticleTable:
         async with self.db.transaction():
             # Подготовка данных для пакетной вставки в vendor_mapping
             article_data = [(nm_id, data[nm_id]['account'], data[nm_id]['vendor_code'], data[nm_id]['wild'])
-                            for nm_id, in filter_nm_ids]
+                            for nm_id in filter_nm_ids]
 
             print(article_data)
             # Пакетная вставка в vendor_mapping
             vendor_mapping_query = """
             INSERT INTO article (nm_id, account, vendor_code, local_vendor_code)
-            VALUES ($1, $2, $3, $4) ;
+            VALUES ($1, $2, $3, $4) 
+            ON CONFLICT (account, vendor_code) DO NOTHING;
             """
-            await self.db.executemany(vendor_mapping_query, article_data)
+            # await self.db.executemany(vendor_mapping_query, article_data)
+
+        for i, (nm_id, account, vendor_code, wild) in enumerate(article_data):
+            if vendor_code == "не найдено":
+                new_vendor_code = vendor_code
+                counter = 0
+                while True:
+                    # Проверяем, существует ли уже такой vendor_code
+                    check_query = """
+                    SELECT 1 FROM article WHERE account = $1 AND vendor_code = $2;
+                    """
+                    result = await self.db.fetch(check_query, account, new_vendor_code)
+                    if not result:
+                        # Если такого vendor_code нет, используем его
+                        break
+                    counter += 1
+                    new_vendor_code = f"{vendor_code}{counter}"
+
+                # Обновляем vendor_code в данных
+                article_data[i] = (nm_id, account, new_vendor_code, wild)
+
+        await self.db.executemany(vendor_mapping_query, article_data)
