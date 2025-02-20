6045a4244ea80a6682a879a2c4c7468cb4239d82
SKurbick <kurban.63.axmedov@gmail.com>
2025-02-20 10:01:16 +0300

    [+] асинхронный таскер

--- main.py
+++ main.py
@@ -119,72 +119,90 @@ async def check_edits_columns():
 # async def run_in_executor(func, *args):
 #     loop = asyncio.get_event_loop()
 #     return await loop.run_in_executor(None, func, *args)
-
-
-def schedule_tasks():
-    gs_service = gs_service_for_schedule_connection()
-
-    "Добавляет выручку и сдвигает столбцы с выручкой по необходимости. Условие должно работать  каждые 25 мин"
-    # schedule.every(25).minutes.do(lambda: asyncio.create_task(gs_service.add_new_day_revenue_to_table()))
-
-    """Актуализация информации по ценам, скидкам, габаритам, комиссии, логистики от склада WB до ПВЗ"""
-    schedule.every(800).seconds.do(lambda: asyncio.create_task(gs_service.add_actually_data_to_table()))
-
-    """Смотрит в таблицу, оценивает новые nm_ids"""
-    schedule.every(300).seconds.do(lambda: asyncio.create_task(check_new_nm_ids()))
-
-    """Смотрит в таблицу, оценивает изменения"""
-    schedule.every(250).seconds.do(lambda: asyncio.create_task(check_edits_columns()))
-
-    """проверяет остатки, обновляет через Сопост"""
-    schedule.every(20).minutes.do(lambda: asyncio.create_task(gs_service.check_quantity_flag()))
-
-    """актуализация остатков по регионам в таблице MAIN"""
-    schedule.every(5).minutes.do(lambda: asyncio.create_task(gs_service.get_actually_data_by_qty()))
-
-    """Выгрузка данных по обороту"""
-    schedule.every().day.at("00:01:55").do(lambda: asyncio.create_task(gs_service.turnover_of_goods()))
-    schedule.every().day.at("09:30:30").do(lambda: asyncio.create_task(gs_service.actualize_avg_orders_data_in_table()))
-    """Актуализация данных по выручке, заказам и сумме с чистой прибыли"""
-    schedule.every(6).minutes.do(lambda: asyncio.create_task(gs_service.get_actually_revenues_orders_and_net_profit_data()))
-    """Смотрим состояние заголовков текущих дней"""
-    schedule.every(30).minutes.do(lambda: asyncio.create_task(gs_service.check_headers()))
-
-
-async def run_scheduler():
-    while True:
-        schedule.run_pending()
-        await asyncio.sleep(1)
-
-async def main():
-    schedule_tasks()
-    await run_scheduler()
-
-
-# from apscheduler.schedulers.asyncio import AsyncIOScheduler
 #
 #
-# async def main():
+# def schedule_tasks():
 #     gs_service = gs_service_for_schedule_connection()
 #
-#     # Create an instance of the scheduler
-#     scheduler = AsyncIOScheduler()
+#     "Добавляет выручку и сдвигает столбцы с выручкой по необходимости. Условие должно работать  каждые 25 мин"
+#     # schedule.every(25).minutes.do(lambda: asyncio.create_task(gs_service.add_new_day_revenue_to_table())) #  устарел
 #
-#     # Add jobs with the appropriate triggers and intervals
-#     scheduler.add_job(gs_service.add_new_day_revenue_to_table, 'interval', minutes=25)
-#     scheduler.add_job(gs_service.add_actually_data_to_table, 'interval', seconds=800)
-#     scheduler.add_job(check_new_nm_ids, 'interval', seconds=300)
-#     scheduler.add_job(check_edits_columns, 'interval', seconds=250)
-#     scheduler.add_job(gs_service.check_quantity_flag, 'interval', minutes=20)
-#     # Uncomment the following line to add the daily job
-#     # scheduler.add_job(update_purchase_calculation_data, 'cron', hour=9, minute=30)
+#     """Актуализация информации по ценам, скидкам, габаритам, комиссии, логистики от склада WB до ПВЗ"""
+#     schedule.every(800).seconds.do(lambda: asyncio.create_task(gs_service.add_actually_data_to_table()))
 #
-#     # Start the scheduler
-#     scheduler.start()
+#     """Смотрит в таблицу, оценивает новые nm_ids"""
+#     schedule.every(300).seconds.do(lambda: asyncio.create_task(check_new_nm_ids()))
 #
-#     # Keep the event loop running indefinitely
-#     event = asyncio.Event()
-#     await event.wait()
+#     """Смотрит в таблицу, оценивает изменения"""
+#     schedule.every(250).seconds.do(lambda: asyncio.create_task(check_edits_columns()))
+#
+#     """проверяет остатки, обновляет через Сопост"""
+#     schedule.every(20).minutes.do(lambda: asyncio.create_task(gs_service.check_quantity_flag()))
+#
+#     """актуализация остатков по регионам в таблице MAIN"""
+#     schedule.every(5).minutes.do(lambda: asyncio.create_task(gs_service.get_actually_data_by_qty()))
+#
+#     """Выгрузка данных по обороту"""
+#     schedule.every().day.at("00:01:55").do(lambda: asyncio.create_task(gs_service.turnover_of_goods()))
+#     schedule.every().day.at("09:30:30").do(lambda: asyncio.create_task(gs_service.actualize_avg_orders_data_in_table()))
+#     """Актуализация данных по выручке, заказам и сумме с чистой прибыли"""
+#     schedule.every(6).minutes.do(lambda: asyncio.create_task(gs_service.get_actually_revenues_orders_and_net_profit_data()))
+#     """Смотрим состояние заголовков текущих дней"""
+#     schedule.every(30).minutes.do(lambda: asyncio.create_task(gs_service.check_headers()))
+#
+#
+# async def run_scheduler():
+#     while True:
+#         schedule.run_pending()
+#         await asyncio.sleep(1)
+
+
+# async def main():
+#     schedule_tasks()
+#     await run_scheduler()
+
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+
+async def main():
+    gs_service = gs_service_for_schedule_connection()
+
+    # Create an instance of the scheduler
+    # scheduler = AsyncIOScheduler()
+    scheduler = AsyncIOScheduler(job_defaults={'misfire_grace_time': 1000, 'max_instances': 1})
+
+    """Актуализация данных по выручке, заказам и сумме с чистой прибыли"""
+    scheduler.add_job(gs_service.get_actually_revenues_orders_and_net_profit_data, 'interval', minutes=6, coalesce=True)
+
+    """Актуализация информации по ценам, скидкам, габаритам, комиссии, логистики от склада WB до ПВЗ"""
+    scheduler.add_job(gs_service.add_actually_data_to_table, 'interval', seconds=800, coalesce=True)
+
+    """Смотрит в таблицу, оценивает новые nm_ids"""
+    scheduler.add_job(check_new_nm_ids, 'interval', seconds=300, coalesce=True)
+
+    """Смотрит в таблицу, оценивает изменения"""
+    scheduler.add_job(check_edits_columns, 'interval', seconds=250, coalesce=True)
+
+    """проверяет остатки, обновляет через Сопост"""
+    scheduler.add_job(gs_service.check_quantity_flag, 'interval', minutes=20, coalesce=True)
+
+    """Выгрузка данных по обороту"""
+    scheduler.add_job(gs_service.actualize_avg_orders_data_in_table, 'cron', hour=9, minute=30)
+    scheduler.add_job(gs_service.turnover_of_goods, 'cron', hour=1, minute=55)
+
+    """Смотрим состояние заголовков текущих дней"""
+    scheduler.add_job(gs_service.check_headers, 'interval', minutes=30, coalesce=True)
+
+    """актуализация остатков по регионам в таблице MAIN"""
+    scheduler.add_job(gs_service.get_actually_data_by_qty, 'interval', minutes=5, coalesce=True)
+
+    # Start the scheduler
+    scheduler.start()
+
+    # Keep the event loop running indefinitely
+    event = asyncio.Event()
+    await event.wait()
 
 
 if __name__ == "__main__":
