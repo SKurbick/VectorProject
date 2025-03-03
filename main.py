267836d4192dcb8ae6b76d0d1417ac93841d05e0
uventus <uventus_work@mail.ru>
2025-03-03 11:34:15 +0300

    add : Добавление loguru

--- main.py
+++ main.py
@@ -171,26 +171,26 @@ async def check_edits_columns():
 #     await run_scheduler()
 
 
-# @scheduler.scheduled_job(IntervalTrigger(minutes=6), coalesce=True)
-# @log_job
-# async def get_actually_revenues_orders_and_net_profit_data():
-#     """Актуализация данных по выручке, заказам и сумме с чистой прибыли"""
-#     logger.info("Запуск : Актуализация данных по выручке, заказам и сумме с чистой прибыли")
-#     gs_service = gs_service_for_schedule_connection()
-#     await gs_service.get_actually_revenues_orders_and_net_profit_data()
-#     logger.info("Завершение : Актуализация данных по выручке, заказам и сумме с чистой прибыли")
+@scheduler.scheduled_job(IntervalTrigger(minutes=6), coalesce=True)
+@log_job
+async def get_actually_revenues_orders_and_net_profit_data():
+    """Актуализация данных по выручке, заказам и сумме с чистой прибыли"""
+    logger.info("Запуск : Актуализация данных по выручке, заказам и сумме с чистой прибыли")
+    gs_service = gs_service_for_schedule_connection()
+    await gs_service.get_actually_revenues_orders_and_net_profit_data()
+    logger.info("Завершение : Актуализация данных по выручке, заказам и сумме с чистой прибыли")
 
 
-# @scheduler.scheduled_job(IntervalTrigger(seconds=800), coalesce=True)
-# @log_job
-# async def add_actually_data_to_table():
-#     """Актуализация информации по ценам, скидкам, габаритам, комиссии, логистики от склада WB до ПВЗ"""
-#     logger.info(
-#         "Запуск : Актуализация информации по ценам, скидкам, габаритам, комиссии, логистики от склада WB до ПВЗ")
-#     gs_service = gs_service_for_schedule_connection()
-#     await gs_service.add_actually_data_to_table()
-#     logger.info(
-#         "Завершение : Актуализация информации по ценам, скидкам, габаритам, комиссии, логистики от склада WB до ПВЗ")
+@scheduler.scheduled_job(IntervalTrigger(seconds=800), coalesce=True)
+@log_job
+async def add_actually_data_to_table():
+    """Актуализация информации по ценам, скидкам, габаритам, комиссии, логистики от склада WB до ПВЗ"""
+    logger.info(
+        "Запуск : Актуализация информации по ценам, скидкам, габаритам, комиссии, логистики от склада WB до ПВЗ")
+    gs_service = gs_service_for_schedule_connection()
+    await gs_service.add_actually_data_to_table()
+    logger.info(
+        "Завершение : Актуализация информации по ценам, скидкам, габаритам, комиссии, логистики от склада WB до ПВЗ")
 
 
 @scheduler.scheduled_job(IntervalTrigger(seconds=300), coalesce=True)
